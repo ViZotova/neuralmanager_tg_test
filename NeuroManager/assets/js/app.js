f4dc401637a64069ef22be4f674bc77d46d3d06e
Mikhail Poladov <mixael.poladof@mail.ru>
2025-08-30 15:21:52 +0300

    Fixed Telegram CSS Issues

--- NeuroManager/assets/js/app.js
+++ NeuroManager/assets/js/app.js
@@ -1,18 +1,17 @@
-// window.addEventListener('load', (event) => {
-//     const tg = window.Telegram.WebApp;
-//     tg.ready(); // Подготовка WebApp
-//     tg.expand(); // Разворачиваем WebApp
-//     tg.disableVerticalSwipes();
+window.addEventListener('load', (event) => {
+    const tg = window.Telegram.WebApp;
+    tg.ready(); // Подготовка WebApp
+    tg.expand(); // Разворачиваем WebApp
+    tg.disableVerticalSwipes();
 
-//     // Сохраняем userId в localStorage
-//     const userId = tg.initDataUnsafe.user?.id;
-//     if (userId) {
-//         localStorage.setItem("userId", userId);
-//         console.log("User ID сохранен:", userId);
-//     } else {
-//         console.error("User ID не найден в initDataUnsafe.");
-//     }
-// });
+    // Сохраняем userId в localStorage
+    const userId = tg.initDataUnsafe.user?.id;
+    if (userId) {
+        localStorage.setItem("userId", userId);
+        console.log("User ID сохранен:", userId);
+    } else {
+        console.error("User ID не найден в initDataUnsafe.");
+    }
+});
 
 let currentChatId = -1;
-
