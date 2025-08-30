72bade629b12c2513f0aac38f4c2a786aff21e09
Mikhail Poladov <mixael.poladof@mail.ru>
2025-08-30 16:11:48 +0300

    TG ID Removed

--- NeuroManager/assets/js/app.js
+++ NeuroManager/assets/js/app.js
@@ -6,11 +6,10 @@ window.addEventListener('load', (event) => {
 
     // Сохраняем userId в localStorage
     const userId = tg.initDataUnsafe.user?.id;
-    document.querySelector("#tg_id_value").innerHTML = userId;
-    
+
     if (userId) {
         localStorage.setItem("userId", userId);
-        console.log("User ID сохранен:", userId);
+        alert("User ID сохранен:", userId);
     } else {
         console.error("User ID не найден в initDataUnsafe.");
     }
