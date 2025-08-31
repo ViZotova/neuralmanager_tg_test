14b793629daaf0fc2788a2d85dc4bcec6cb5ff4a
Mikhail <mixael.poladof@mail.ru>
2025-08-31 13:14:56 +0300

    Telegram UID Adding Attempt #2

--- NeuroManager/assets/js/app.js
+++ NeuroManager/assets/js/app.js
@@ -4,8 +4,8 @@ menuButton.addEventListener('click', () => {
     try {
         const userId = window.Telegram.WebApp.initDataUnsafe?.user?.id || localStorage.getItem('userId');
     }
-    catch {
-        alert("Запустите приложение в Telegram");
+    catch (e) {
+        alert(e);
     }
     alert(userId);
 });
