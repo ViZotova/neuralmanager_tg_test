f2c21ed459ebb82c8acb5028bc65fbfd190e88ed
Mikhail Poladov <mixael.poladof@mail.ru>
2025-08-31 11:31:31 +0300

    Telegram Part Removed

--- NeuroManager/assets/js/app.js
+++ NeuroManager/assets/js/app.js
@@ -1,19 +1,19 @@
-window.addEventListener('load', (event) => {
-    const tg = window.Telegram.WebApp;
-    tg.ready(); // Подготовка WebApp
-    tg.expand(); // Разворачиваем WebApp
-    tg.disableVerticalSwipes();
+// window.addEventListener('load', (event) => {
+//     const tg = window.Telegram.WebApp;
+//     tg.ready(); // Подготовка WebApp
+//     tg.expand(); // Разворачиваем WebApp
+//     tg.disableVerticalSwipes();
 
-    // Сохраняем userId в localStorage
-    const userId = tg.initDataUnsafe.user?.id;
-    alert(userId);
+//     // Сохраняем userId в localStorage
+//     const userId = tg.initDataUnsafe.user?.id;
+//     alert(userId);
 
-    if (userId) {
-        localStorage.setItem("userId", userId);
-        alert("User ID сохранен:", userId);
-    } else {
-        console.error("User ID не найден в initDataUnsafe.");
-    }
-});
+//     if (userId) {
+//         localStorage.setItem("userId", userId);
+//         alert("User ID сохранен:", userId);
+//     } else {
+//         console.error("User ID не найден в initDataUnsafe.");
+//     }
+// });
 
 let currentChatId = -1;
